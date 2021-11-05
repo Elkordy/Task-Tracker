63351c9dea4fd751982aacb055336f07eac8d200
El-Kordy <MuhammadElekrady@users.noreply.github.com>
2021-11-05 15:04:00 +0200

    Task Tracker
    Simple application for tracking tasks by using:
    Angular framework and it's features.
    JSON Server

---
+++ src/app/components/task-item/task-item.component.ts
@@ -0,0 +1,26 @@
+import { Task } from './../../Task';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
+@Component({
+  selector: 'app-task-item',
+  templateUrl: './task-item.component.html',
+  styleUrls: ['./task-item.component.css']
+})
+export class TaskItemComponent implements OnInit {
+  @Input() task: Task;
+  @Output() onDeleteTask = new EventEmitter();
+  @Output() onToggleReminder = new EventEmitter();
+
+  faTimes = faTimes;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  onDelete(task: Task) {
+    this.onDeleteTask.emit(task);
+  }
+  onToggle(task: Task) {
+    this.onToggleReminder.emit(task);
+  }
+
+}
